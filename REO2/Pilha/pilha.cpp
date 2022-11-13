5c75b41e21335d6e7977ffc3634248de72631052
gustavo figueiredo <gustavo.figueiredo@aiqfome.com>
2022-11-13 17:37:26 -0300

    feat: implementação pilha

---
+++ REO2/Pilha/pilha.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+
+typedef char Dado;
+
+class Noh { 
+    friend class Pilha;
+    private:
+        Dado mDado;
+        Noh* mPtrProx;
+};
+
+class Pilha {
+    public:
+        Pilha();
+        ~Pilha();
+        void empilhar(const Dado& dado); // ok
+        Dado desempilhar(); // ok
+        void limparTudo(); // ok
+        int getTamanho();
+        Dado espiarTopo() const; // ok
+        bool pilhaVazia(); // ok
+    private:
+        Noh* mPtrTopo;
+        int mTamanho;
+};
+
+Pilha::Pilha() {
+    this->mPtrTopo = NULL;
+    this->mTamanho = 0;
+}
+
+void Pilha::empilhar(const Dado& dado) {
+    Noh* auxiliar = new Noh;
+
+    auxiliar->mDado = dado;
+    auxiliar->mPtrProx = this->mPtrTopo;
+
+    this->mPtrTopo = auxiliar;
+    this->mTamanho++;
+}
+
+Dado Pilha::desempilhar() {
+    if (this->mTamanho = 0) {
+        cout << "Impossível desempilhar!" << endl;
+        return -1;
+    }
+
+    Noh* auxiliar;
+    auxiliar = this->mPtrTopo;
+
+    Dado valor = auxiliar->mDado;
+
+    this->mPtrTopo = auxiliar->mPtrProx;
+    this->mTamanho--;
+
+    delete auxiliar; 
+    return valor;
+}
+
+void Pilha::limparTudo() {
+    // while (this->mPtrTopo != NULL) {} // correto ?
+    while (this->mTamanho > 0) {
+        this->desempilhar();
+    }
+}
+
+int Pilha::getTamanho() {
+    return this->mTamanho;
+}
+
+Dado Pilha::espiarTopo() const {
+    Dado valor = this->mPtrTopo->mDado;
+    return valor;
+}
+
+bool Pilha::pilhaVazia() {
+    return (this->mTamanho == 0);
+}
+
+Pilha::~Pilha() {
+    this->limparTudo();
+}
+
+void mostrarMenu() {
+    cout << "O que você deseja fazer na pilha ?" << endl;
+    cout << "Inserir dado: i" << endl;
+    cout << "Acessar topo: t" << endl;
+    cout << "Desempilhar: d" << endl;
+    cout << "Limpar pilha: l" << endl;
+    cout << "Sair: s" << endl;
+}
+
+int main() {
+    Pilha minhaPilha;
+
+    mostrarMenu();
+    char opcaoMenu;
+    cin >> opcaoMenu;
+
+    while (opcaoMenu != 's') {
+        if (opcaoMenu == 'i') {
+            cout << "Insira algum caracter" << endl;
+            char dado;
+            cin >> dado;
+            minhaPilha.empilhar(dado);
+        }
+        else if (opcaoMenu == 't') {
+            char dado;
+            dado = minhaPilha.espiarTopo();
+            cout << dado << endl;
+        }
+        else if (opcaoMenu == 'd') {
+            char dado;
+            dado = minhaPilha.desempilhar();
+            cout << "desempilhado o caracter: " + dado << endl;
+        }
+        else if (opcaoMenu == 'l') {
+            minhaPilha.limparTudo();
+            cout << "pilha limpa!" << endl;
+        }
+        else if (opcaoMenu == 's') {
+            break;
+        }
+        else {
+            cout << "Opção inválida!" << endl;
+        }
+
+        mostrarMenu();
+        cin >> opcaoMenu;
+    }
+}
