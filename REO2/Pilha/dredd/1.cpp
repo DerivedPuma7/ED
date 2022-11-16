f58ef6e8d2879a6c8fd5fdecbf42685b29e25ce9
gustavo figueiredo <gustavo.figueiredo@aiqfome.com>
2022-11-15 21:14:13 -0300

    feat: questao 1 dreadd - pilha

---
+++ REO2/Pilha/dredd/1.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+typedef int Dado; // para facilitar a troca de int para outro tipo
+
+// pilha implementada em arranjo 
+class pilhav {
+    private:
+        int capacidade;
+        Dado *dados;
+        int tamanho;
+        int posTopo;
+    public:
+        pilhav(int cap = 100);
+        ~pilhav();
+        void empilha(Dado valor);
+        Dado desempilha();
+        Dado espia(); // acessa elemento do topo, mas não retira
+        void depura(); // imprime os dados do vetor da pilha sem removê-los (desempilhar)
+        void info();  // imprime informações da pilha (tamanho e  posição do topo)
+        int getTopo();
+};
+
+int pilhav::getTopo() {
+    return this->posTopo;
+}
+
+pilhav::pilhav(int capacidade) {
+    this->capacidade = capacidade;
+    this->tamanho = 0;
+    this->posTopo = -1;
+    this->dados = new Dado[capacidade];
+}
+
+void pilhav::empilha(Dado valor) {
+    if(this->posTopo == -1) {
+        this->dados[this->posTopo + 1] = valor;
+    }
+    else {
+        this->dados[this->posTopo + 1] = valor;
+    }
+
+    this->posTopo++;
+    this->tamanho++;
+}
+
+Dado pilhav::desempilha() {
+    if(this->posTopo == -1) {
+        return -1;
+    }
+
+    int posicaoDadoTopo = this->posTopo;
+    int dadoTopo = this->dados[posicaoDadoTopo];
+
+    this->dados[this->posTopo] = -1;
+    this->posTopo--;
+    this->tamanho--;
+
+    return dadoTopo;
+}
+
+Dado pilhav::espia() {
+    return this->dados[this->posTopo];
+}
+
+void pilhav::depura() {
+    int posicaoTopo = this->posTopo;
+    while (posicaoTopo >= 0) {
+        cout << this->dados[posicaoTopo] << endl;
+        posicaoTopo--;
+    }
+}
+
+void pilhav::info() {
+    cout << this->tamanho << ' ' << this->posTopo << endl;
+}
+
+pilhav::~pilhav() {
+    int posicaoTopo = this->posTopo;
+    while (posicaoTopo >= 0) {
+        this->desempilha();
+        posicaoTopo--;
+    }
+}
+
+int main () {
+    int capacidadePilha = 20;
+    int valorEmpilhar;
+    pilhav minhaPilha(capacidadePilha);
+
+    cout << "empilha 5" << endl;
+    for(int i = 0; i < 5; i++) {
+        cin >> valorEmpilhar;
+        minhaPilha.empilha(valorEmpilhar);
+    }
+
+    cout << "desempilha 3" << endl;
+    for(int i = 0; i < 3; i++) {
+        cout << minhaPilha.desempilha() << endl;
+    }
+
+    cout << "empilha 4" << endl;
+    for(int i = 0; i < 4; i++) {
+        cin >> valorEmpilhar;
+        minhaPilha.empilha(valorEmpilhar);
+    }
+
+    cout << "desempilha 3" << endl;
+    for(int i = 0; i < 3; i++) {
+        cout << minhaPilha.desempilha() << endl;
+    }
+    cout << "espia" << endl;
+    cout << minhaPilha.espia() << endl;
+
+    cout << "depura" << endl;
+    minhaPilha.depura();
+
+    cout << "info" << endl;
+    minhaPilha.info();
+
+    return -1;
+}
