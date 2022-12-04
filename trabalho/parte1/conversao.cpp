1c491f2ae1a8b2c50b85ac6f1ec05bc3679319ea
gustavo figueiredo <gustavo.figueiredo@aiqfome.com>
2022-12-04 18:17:14 -0300

    refactor: cleaning conversao csv to binary

--- trabalho/parte1/conversao.cpp
+++ trabalho/parte1/conversao.cpp
@@ -35,15 +35,50 @@ struct SubnationalPeriodLifeTables
 	float value;
 };
 
+class LinhaCsv {
+	public:
+		string atributoLinha;
+		string restanteLinha;
+		void tratarLinha(string linhaCsv);
+		float tratarPorcentagem(string atributoLinha);
+};
+
+void LinhaCsv::tratarLinha(string linhaCsv) {
+	string campo;
+	unsigned posFimCampo;
+
+	posFimCampo = linhaCsv.find(',');
+	campo = linhaCsv.substr(0, posFimCampo);
+	linhaCsv.erase(0, posFimCampo + 1);
+
+	this->atributoLinha = campo;
+	this->restanteLinha = linhaCsv;
+}
+
+float LinhaCsv::tratarPorcentagem(string atributoLinha) {
+	int ultimaPosicaoAtributo = atributoLinha.length() - 1;
+	float atributoRetorno;
+
+	if (atributoLinha[ultimaPosicaoAtributo] == '%') {
+		atributoLinha = atributoLinha.substr(0, ultimaPosicaoAtributo);
+		atributoRetorno = stof(atributoLinha) / 100;
+	}
+	else {
+		atributoRetorno = stof(atributoLinha);
+	}
+
+	return atributoRetorno;
+}
+
 int main()
 {
 	ifstream arquivoLeituraCsv("SubnationalPeriodLifeTables.csv");
+	
 	ofstream arquivoEscritaBin;
-
-	string linha;
-	SubnationalPeriodLifeTables registro;
-
 	arquivoEscritaBin.open("SubnationalPeriodLifeTables.bin", ios::binary | ios::out);
+
+	SubnationalPeriodLifeTables registro;
+	LinhaCsv linhaCsv;
 
 	if (!arquivoLeituraCsv)
 	{
@@ -51,77 +86,35 @@ int main()
 		return 0;
 	}
 
-	string campo;
-	unsigned posFimCampo;
-
-	getline(arquivoLeituraCsv, linha);
-	int contador = 0;
-	char c[1];
-	while (getline(arquivoLeituraCsv, linha))
+	getline(arquivoLeituraCsv, linhaCsv.restanteLinha);
+	while (getline(arquivoLeituraCsv, linhaCsv.restanteLinha))
 	{
-		// Obtem registro, texto - Measure
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.measure, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.measure, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, float - Quantile
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		if (campo[campo.length() - 1] == '%')
-		{
-			campo = campo.substr(0, campo.length() - 1);
-			registro.quantile = stof(campo) / 100;
-		}
-		else
-		{
-			registro.quantile = stof(campo);
-		}
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		registro.quantile = linhaCsv.tratarPorcentagem(linhaCsv.atributoLinha);
 
-		// Obtem registro, texto - Area
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.area, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.area, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, texto - Sex
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.sex, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.sex, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, texto - Age
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.age, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.age, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, texto - Geography
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.geography, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.geography, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, texto - Ethnic
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		strcpy(registro.ethnic, campo.c_str());
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		strcpy(registro.ethnic, linhaCsv.atributoLinha.c_str());
 
-		// Obtem registro, texto - Value
-		posFimCampo = linha.find(',');
-		campo = linha.substr(0, posFimCampo);
-		linha.erase(0, posFimCampo + 1);
-		registro.value = stof(campo);
-
-		cout << '.';
+		linhaCsv.tratarLinha(linhaCsv.restanteLinha);
+		registro.value = stof(linhaCsv.atributoLinha);
 
 		arquivoEscritaBin.write((const char *)(&registro), sizeof(SubnationalPeriodLifeTables));
-		;
 	}
-
-	cout << endl;
 
 	arquivoEscritaBin.close();
 	arquivoLeituraCsv.close();
